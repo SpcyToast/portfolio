c89e115c2efb2bab79bd7574454b57e8c8762ffd
Prashniel Kumar <prashniel@gmail.com>
2025-03-02 11:43:50 +1300

    basic tictactoe functional

--- client/components/application 1/Tictactoe.tsx
+++ client/components/application 1/Tictactoe.tsx
@@ -1,5 +1,6 @@
 import getRandomNumber from '@/client/getRandomNumber'
 import { useEffect, useState } from 'react'
+import winStates from '@/client/data/tictactoe.json'
 
 export default function Tictactoe() {
   const cleanBoard: number[] = [2, 2, 2, 2, 2, 2, 2, 2, 2]
@@ -10,20 +11,31 @@ export default function Tictactoe() {
   useEffect(() => {
     reset()
   }, [])
-  
+
   // randomise starting player at the start of every game
   function reset() {
     setBoard(cleanBoard)
     const start = getRandomNumber(0, 1)
     start === 0 ? setTurn(0) : setTurn(1)
+    setWinState(false)
   }
 
   function makeMove(i: number) {
     const play = [...board]
+    let switchable = true
     play[i] = turn
     setBoard(play)
-    console.log(play)
-    switchTurn()
+    for (let i = 0; i < winStates.length; i++) {
+      if (
+        play[winStates[i][0]] === turn &&
+        play[winStates[i][1]] === turn &&
+        play[winStates[i][2]] === turn
+      ) {
+        setWinState(true)
+        switchable = false
+      }
+    }
+    switchable && switchTurn()
   }
 
   function switchTurn() {
@@ -37,9 +49,9 @@ export default function Tictactoe() {
           <button
             className={`item ${'item-' + (i + 1)}} tictactoe`}
             key={i}
-            onClick={() => cell === 2 && makeMove(i)}
+            onClick={() => cell === 2 && !winState && makeMove(i)}
           >
-            {cell === 0 ? 'O' : cell === 1 ? 'X' : 'Testing'}{' '}
+            {cell === 0 ? 'O' : cell === 1 ? 'X' : ''}
           </button>
         )
       })}
