9689e4a7ee75e4a0519a703b3fa1aca0da018ad2
Prashniel Kumar <prashniel@gmail.com>
2025-03-02 18:29:10 +1300

    add focus on icons

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,26 @@ import { useState } from 'react'
 
 export default function Home() {
   const [tictactoe, setTictactoe] = useState(false)
+  const [focus, setFocus] = useState('')
+
+  function useFocus(icon: string) {
+    switch (icon) {
+      case 'Tictactoe':
+        setTictactoe(true)
+        setFocus('')
+        break
+    }
+  }
 
   return (
     <div className="homepage">
-      <button onClick={() => setTictactoe(true)}>
+      <button
+        className={`${focus === 'Tictactoe' && 'focus'}`}
+        onClick={() =>
+          focus === 'Tictactoe' ? useFocus('Tictactoe') : setFocus('Tictactoe')
+        }
+        onDoubleClick={() => setTictactoe(true)}
+      >
         <img src="/tictactoe.svg" className="icon" />
         <p style={{ justifySelf: 'left', fontSize: '10px' }}>Tic-Tac-Toe</p>
       </button>
