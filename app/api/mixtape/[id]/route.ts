40d0a91f206c37a0b64a9b603df118f2cf56c121
Prashniel Kumar <prashniel@gmail.com>
2025-03-07 22:00:28 +1300

    parmasean

--- app/api/mixtape/[id]/route.ts
+++ app/api/mixtape/[id]/route.ts
@@ -1,9 +1,14 @@
 import { turso } from '@/server/database'
 
-export const GET = async (
-  req: Request,
-  { params }: { params: { id: string } }
-) => {
+interface Params {
+  params: Package
+}
+
+interface Package {
+  id: number
+}
+
+export const GET = async (req: Request, { params }: Params) => {
   const jointTable =
     'FROM mixtape INNER JOIN mixtape_songs ON mixtape.id = mixtape_songs.mixtape_id INNER JOIN songs ON mixtape_songs.song_id = songs.id'
   try {
