8e2e6b7d74e67730bd91043f19077f3852c0cc51
Prashniel Kumar <prashniel@gmail.com>
2025-03-08 11:43:08 +1300

    deploy

--- client/components/windows/mixtape/Cassette.tsx
+++ client/components/windows/mixtape/Cassette.tsx
@@ -1,3 +1,11 @@
+import '@/client/styles/cassette.css'
+
 export default function Cassette() {
-  return <>Cassette</>
+  return (
+    <>
+      <div className="cassette-container">
+        <div className="cassette-box">Test</div>
+      </div>
+    </>
+  )
 }
