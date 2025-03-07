0e13516ce7e9951272e0e0f2726d91e336dd0972
Prashniel Kumar <prashniel@gmail.com>
2025-03-07 22:03:22 +1300

    parmesan

--- app/api/mixtape/[id]/route.ts
+++ app/api/mixtape/[id]/route.ts
@@ -1,19 +1,11 @@
 import { turso } from '@/server/database'
 
-interface Params {
-  params: Package
-}
-
-interface Package {
-  id: number
-}
-
-export const GET = async (req: Request, { params }: Params) => {
+export const GET = async (req: Request) => {
   const jointTable =
     'FROM mixtape INNER JOIN mixtape_songs ON mixtape.id = mixtape_songs.mixtape_id INNER JOIN songs ON mixtape_songs.song_id = songs.id'
   try {
     const mixtape = await turso.execute(
-      `SELECT * ${jointTable} WHERE mixtape.id = ${params.id}`
+      `SELECT * ${jointTable} WHERE mixtape.id = ${1}`
     )
 
     return new Response(JSON.stringify(mixtape.rows), { status: 200 })
