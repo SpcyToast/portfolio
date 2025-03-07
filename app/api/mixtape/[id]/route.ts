ade0ddc287eacd911b6f8a806e9c57826b2e0343
Prashniel Kumar <prashniel@gmail.com>
2025-03-07 21:25:45 +1300

    add database route for joined table mixtapes

---
+++ app/api/mixtape/[id]/route.ts
@@ -0,0 +1,18 @@
+import { turso } from '@/server/database'
+
+export const GET = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  const jointTable =
+    'FROM mixtape INNER JOIN mixtape_songs ON mixtape.id = mixtape_songs.mixtape_id INNER JOIN songs ON mixtape_songs.song_id = songs.id'
+  try {
+    const mixtape = await turso.execute(
+      `SELECT * ${jointTable} WHERE mixtape.id = ${params.id}`
+    )
+
+    return new Response(JSON.stringify(mixtape.rows), { status: 200 })
+  } catch (error) {
+    return new Response('Failed to fetch projects', { status: 500 })
+  }
+}
