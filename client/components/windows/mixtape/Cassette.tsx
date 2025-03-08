34490887709e606ff8f719f4e225171c8a377d20
Prashniel Kumar <prashniel@gmail.com>
2025-03-08 22:17:30 +1300

    basic cassette shape

--- client/components/windows/mixtape/Cassette.tsx
+++ client/components/windows/mixtape/Cassette.tsx
@@ -4,7 +4,17 @@ export default function Cassette() {
   return (
     <>
       <div className="cassette-container">
-        <div className="cassette-box">Test</div>
+        <div className="cassette-case"></div>
+        <div className="cassette-label"></div>
+        <div className="cassette-hole-tl"></div>
+        <div className="cassette-hole-tr"></div>
+        <div className="cassette-hole-bl"></div>
+        <div className="cassette-hole-br"></div>
+        <div className="cassette-small-hole-tl"></div>
+        <div className="cassette-small-hole-tr"></div>
+        <div className="cassette-small-hole-bl"></div>
+        <div className="cassette-small-hole-br"></div>
+        <div className="cassette-mechanisim-container"></div>
       </div>
     </>
   )
