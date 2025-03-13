46beb324556608b691dc4c8745d6e629040be0a3
Prashniel Kumar <prashniel@gmail.com>
2025-03-14 00:00:00 +1300

    link default values to each div in cassette

--- client/components/windows/mixtape/Cassette.tsx
+++ client/components/windows/mixtape/Cassette.tsx
@@ -29,104 +29,104 @@ export default function Cassette({
     image: image,
   })
 
-  useEffect(() => {
-    // default values for new tape or unaltered colours
-    const defaultAttributes = { ...attributes }
-
-    if (defaultAttributes.font_colour === null) {
-      defaultAttributes.font_colour = 'rgb(0, 0, 0)'
-    }
-    if (defaultAttributes.A === null) {
-      defaultAttributes.A = 'rgb(0, 0, 0)'
-    }
-    if (defaultAttributes.B === null) {
-      defaultAttributes.B = 'rgb(134, 134, 131)'
-    }
-    if (defaultAttributes.C === null) {
-      defaultAttributes.C = 'rgb(51, 51, 51)'
-    }
-    if (defaultAttributes.D === null) {
-      defaultAttributes.D = 'rgb(29, 29, 29)'
-    }
-    if (defaultAttributes.E === null) {
-      defaultAttributes.E = 'rgb(116, 116, 116)'
-    }
-    if (defaultAttributes.F === null) {
-      defaultAttributes.F = 'rgb(0, 0, 0)'
-    }
-    if (defaultAttributes.G === null) {
-      defaultAttributes.G = 'rgb(90, 88, 88)'
-    }
-    console.log(defaultAttributes)
-
-    setAttributes(defaultAttributes)
-  }, [])
-
   return (
     <>
       <div className="cassette-container">
         <div
           className="cassette-case"
-          style={{ backgroundColor: attributes.A }}
+          style={{
+            backgroundColor: attributes.A ? attributes.A : 'rgb(0, 0, 0)',
+          }}
         ></div>
         <div
           className="cassette-label"
-          style={{ backgroundColor: attributes.B }}
+          style={{
+            backgroundColor: attributes.B ? attributes.B : 'rgb(134, 134, 131)',
+          }}
         >
           {attributes.image !== null && (
             <img src={attributes.image} className="cassette-image" />
           )}
           <h1
             className="cassette-name"
-            style={{ color: attributes.font_colour, font: attributes.font }}
+            style={{
+              color: attributes.font_colour
+                ? attributes.font_colour
+                : 'rgb(0, 0, 0)',
+              font: attributes.font
+                ? attributes.font
+                : 'Arial, Helvetica, sans-serif',
+            }}
           >
             {attributes.name}
           </h1>
         </div>
         <div
           className="cassette-hole-tl"
-          style={{ backgroundColor: attributes.C }}
+          style={{
+            backgroundColor: attributes.C ? attributes.C : 'rgb(51, 51, 51)',
+          }}
         ></div>
         <div
           className="cassette-hole-tr"
-          style={{ backgroundColor: attributes.C }}
+          style={{
+            backgroundColor: attributes.C ? attributes.C : 'rgb(51, 51, 51)',
+          }}
         ></div>
         <div
           className="cassette-hole-bl"
-          style={{ backgroundColor: attributes.C }}
+          style={{
+            backgroundColor: attributes.C ? attributes.C : 'rgb(51, 51, 51)',
+          }}
         ></div>
         <div
           className="cassette-hole-br"
-          style={{ backgroundColor: attributes.C }}
+          style={{
+            backgroundColor: attributes.C ? attributes.C : 'rgb(51, 51, 51)',
+          }}
         ></div>
         <div
           className="cassette-small-hole-tl"
-          style={{ backgroundColor: attributes.F }}
+          style={{
+            backgroundColor: attributes.F ? attributes.F : 'rgb(0, 0, 0)',
+          }}
         ></div>
         <div
           className="cassette-small-hole-tr"
-          style={{ backgroundColor: attributes.F }}
+          style={{
+            backgroundColor: attributes.F ? attributes.F : 'rgb(0, 0, 0)',
+          }}
         ></div>
         <div
           className="cassette-small-hole-bl"
-          style={{ backgroundColor: attributes.F }}
+          style={{
+            backgroundColor: attributes.F ? attributes.F : 'rgb(0, 0, 0)',
+          }}
         ></div>
         <div
           className="cassette-small-hole-br"
-          style={{ backgroundColor: attributes.F }}
+          style={{
+            backgroundColor: attributes.F ? attributes.F : 'rgb(0, 0, 0)',
+          }}
         ></div>
         <div
           className="cassette-mechanisim-container"
-          style={{ backgroundColor: attributes.D }}
+          style={{
+            backgroundColor: attributes.D ? attributes.D : 'rgb(29, 29, 29)',
+          }}
         ></div>
         <div className="cassette-mechanisim-reel">
           <div
             className="reel-in"
-            style={{ backgroundColor: attributes.G }}
+            style={{
+              backgroundColor: attributes.G ? attributes.G : 'rgb(90, 88, 88)',
+            }}
           ></div>
           <div
             className="reel-out"
-            style={{ backgroundColor: attributes.G }}
+            style={{
+              backgroundColor: attributes.G ? attributes.G : 'rgb(90, 88, 88)',
+            }}
           ></div>
         </div>
         <div className="cassette-mechanisim-cogslot-l">
@@ -137,25 +137,35 @@ export default function Cassette({
         </div>
         <div
           className="cassette-bottom-box"
-          style={{ backgroundColor: attributes.E }}
+          style={{
+            backgroundColor: attributes.E ? attributes.E : 'rgb(116, 116, 116)',
+          }}
         ></div>
         <div
           className="cassette-bottom-box-l"
-          style={{ backgroundColor: attributes.E }}
+          style={{
+            backgroundColor: attributes.E ? attributes.E : 'rgb(116, 116, 116)',
+          }}
         ></div>
         <div
           className="cassette-bottom-box-r"
-          style={{ backgroundColor: attributes.E }}
+          style={{
+            backgroundColor: attributes.E ? attributes.E : 'rgb(116, 116, 116)',
+          }}
         ></div>
         <div className="cassette-bottom-hole-l"></div>
         <div className="cassette-bottom-hole-r"></div>
         <div
           className="cassette-bottom-hole-c"
-          style={{ backgroundColor: attributes.C }}
+          style={{
+            backgroundColor: attributes.C ? attributes.C : 'rgb(51, 51, 51)',
+          }}
         ></div>
         <div
           className="cassette-bottom-hole-c-small"
-          style={{ backgroundColor: attributes.F }}
+          style={{
+            backgroundColor: attributes.F ? attributes.F : 'rgb(0, 0, 0)',
+          }}
         ></div>
         <div className="cassette-bottom-nubb-l"></div>
         <div className="cassette-bottom-nubb-r"></div>
